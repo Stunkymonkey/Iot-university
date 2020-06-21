6398129def9fe0dfbff51ea22a7330ca352ed112
Felix Buehler <account@buehler.rocks>
2020-06-21 17:18:27 +0200

    finish key value store for database

--- uberspace/database.py
+++ uberspace/database.py
@@ -3,13 +3,13 @@ import sqlite3
 conn = sqlite3.connect('IoT.db')
 
 c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS sensors (key TEXT PRIMARY KEY, value INT DEFAULT 0)''')
-c.execute('''CREATE TABLE IF NOT EXISTS actuators (key TEXT PRIMARY KEY, value INT DEFAULT 0)''')
+c.execute('''CREATE TABLE IF NOT EXISTS sensor (key TEXT PRIMARY KEY, value INT DEFAULT 0)''')
+c.execute('''CREATE TABLE IF NOT EXISTS actuator (key TEXT PRIMARY KEY, value INT DEFAULT 0)''')
 conn.commit()
 
 
 def upsert(table, key, value):
-    c.execute("INSERT INTO " + table + "(key) VALUES('" + key + "') ON CONFLICT(key) DO UPDATE SET value=" + value + ";")
+    c.execute("INSERT INTO " + table + "(key, value) VALUES('" + key + "', " + value + ") ON CONFLICT(key) DO UPDATE SET value=" + value + ";")
     conn.commit()
 
 
@@ -21,3 +21,8 @@ def increment(table, key):
 def decrement(table, key):
     c.execute("INSERT INTO " + table + "(key) VALUES('" + key + "') ON CONFLICT(key) DO UPDATE SET value=value-1;")
     conn.commit()
+
+
+def get(table, key):
+    c.execute("SELECT value FROM " + table + ";")
+    return c.fetchone()[0]
--- uberspace/planer.py
+++ uberspace/planer.py
@@ -3,36 +3,40 @@
 import paho.mqtt.client as mqtt
 import database
 import export
+import time
+
 
 def on_connect(client, userdata, flags, rc):
   print('client connected')
   client.subscribe("iot/#")
+
 
 def publishLED():
   mqttPublish.sendValue(states.getCount(0), 'section1/led')
   mqttPublish.sendValue(states.getCount(1), 'section2/led')
   print('publishLED')
 
+
 def on_message(client, userdata, msg):
-  print(msg.topic + ' '  +msg.payload.decode())
+  print(msg.topic + ' ' + msg.payload.decode())
   states.setStates(msg.topic, msg.payload.decode())
-  if (msg.topic == 'iot/section1/button/in') and (msg.payload.decode() =='1'):
+  if (msg.topic == 'iot/section1/button/in') and (msg.payload.decode() == '1'):
     states.setCount(0, states.getCount(0) + 1)
     publishLED()
   if (msg.topic == 'iot/section2/button/in') and (msg.payload.decode() == '1'):
     states.setCount(1, states.getCount(1) + 1)
     publishLED()
   if (msg.topic == 'iot/section1/button/out') and (msg.payload.decode() == '1'):
-    states.setCount(0, states.getCount(0) - 1 )
+    states.setCount(0, states.getCount(0) - 1)
     publishLED()
   if (msg.topic == 'iot/section2/button/out') and (msg.payload.decode() == '1'):
     states.setCount(1, states.getCount(1) - 1)
     publishLED()
   print(states.personCount)
-  #client.disconnect()
+  # client.disconnect()
 
-#close the connection
-#conn.close()
+# close the connection
+# conn.close()
 
 client = mqtt.Client()
 client.connect("boethin.uberspace.de", 46980, 60)
@@ -40,6 +44,16 @@ client.connect("boethin.uberspace.de", 46980, 60)
 client.on_connect = on_connect
 client.on_message = on_message
 
-export.to_problem_file(1, 2, 3, 4, 5)
-client.loop_forever()
 
+# database.upsert("sensor", "iot/sensors/section1/shelf", "12")
+# time.sleep(1)
+# database.increment("sensor", "iot/sensors/section1/shelf")
+# time.sleep(1)
+# database.increment("sensor", "iot/sensors/section1/shelf")
+# time.sleep(1)
+# database.decrement("sensor", "iot/sensors/section1/shelf")
+# print(database.get("sensor", "iot/sensors/section1/shelf"))
+
+# export.to_problem_file(1, 2, 3, 4, 5)
+
+client.loop_forever()
