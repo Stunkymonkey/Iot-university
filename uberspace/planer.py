f09797571b56dbea6869498da4c3f11327831cbb
Felix Buehler <account@buehler.rocks>
2020-06-22 13:41:26 +0200

    remove old unused code

--- uberspace/planer.py
+++ uberspace/planer.py
@@ -13,14 +13,6 @@ person_counter_0 = "iot/sensors/section0/counter"
 person_counter_1 = "iot/sensors/section1/counter"
 person_counter_2 = "iot/sensors/section2/counter"
 
-actuator_states = list()
-actuator_states.append("iot/actuators/section0/ventilator")
-actuator_states.append("iot/actuators/section1/refill_shelf")
-actuator_states.append("iot/actuators/section2/refill_shelf")
-actuator_states.append("iot/actuators/section0/gate")
-actuator_states.append("iot/actuators/section1/gate")
-actuator_states.append("iot/actuators/section2/gate")
-
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT-Server")
@@ -48,16 +40,6 @@ def save_states(topic, payload):
             database.decrement("sensor", person_counter_2)
     else:
         database.upsert("sensor", topic, payload)
-
-
-def get_old_actuator_states():
-    old_ventilator_on = database.get("actuator", actuator_states[0])
-    old_refill_shelf_1 = database.get("actuator", actuator_states[1])
-    old_refill_shelf_2 = database.get("actuator", actuator_states[2])
-    old_block_0 = database.get("actuator", actuator_states[3])
-    old_block_1 = database.get("actuator", actuator_states[4])
-    old_block_2 = database.get("actuator", actuator_states[5])
-    return [old_ventilator_on, old_refill_shelf_1, old_refill_shelf_2, old_block_0, old_block_1, old_block_2]
 
 
 def on_message(client, userdata, msg):
